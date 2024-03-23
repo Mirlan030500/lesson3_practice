ce5f055fe5133a0bc83cf6fe64ee343a4ee39f44
mirlan030500 <usubalievmirlan.030500@gmail.com>
2024-03-23 23:27:13 +0600

    first commit

---
+++ lesson3_practice.dart
@@ -0,0 +1,35 @@
+void main(){
+
+int w = 2;
+int z = 2;
+print(w+z);
+int c = 5;
+
+print(c -= 10);
+int g = 16 * 3;
+print(g);
+int d = 16~/2;
+print(d);
+
+int a = 12;
+int b =15;
+print(a>b);
+print(a<b);
+print(a<=b);
+print(a>=b);
+print(a==b);
+print(a!=b);
+
+double t = 20.2;
+print(t.toInt());
+double y = 30.2;
+print(y.toInt());
+int o = 59;
+print(o.toDouble());
+int p = 34;
+print(p.toDouble());
+
+
+
+
+}
